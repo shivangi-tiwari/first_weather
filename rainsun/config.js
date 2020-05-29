891dba9465cb5a667d129ec494eee803823e00b1
unknown <shivangi22july@gmail.com>
2020-05-30 02:21:50 +0530

    weather app

---
+++ rainsun/config.js
@@ -0,0 +1,9 @@
+const constants = {
+    openWeatherMap: { 
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "bbd5887f0c3fec7c11e3906038b9739d"
+    }
+}
+
+//exporting it  as it has weather data
+module.exports = constants;
